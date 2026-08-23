a32bd7131dbe64948ee2deebd9d411430efaa125
ryduan1 <ryduare@gmail.com>
2026-08-23 16:23:51 -0300

    Update app.py

--- app.py
+++ app.py
@@ -280,8 +280,7 @@ st.markdown(
     """
     <div class="custom-footer">
         <b>Desarrollado para la Cátedra de Estática y Resistencia de los Materiales (Cód.0318)</b><br>
-        Facultad de Ingeniería — Universidad Nacional de Río Cuarto (UNRC)<br><br>
-        <b>Contacto:</b> <a href="mailto:ryduare@gmail.com">ryduare@gmail.com</a>
+        Facultad de Ingeniería — Universidad Nacional de Río Cuarto (UNRC)<br><br> <b>Contacto:</b> <a href="mailto:regalum@ing.unrc.edu.ar">regalum@ing.unrc.edu.ar</a>
         <b>Autor:</b> Estudiante de Ingeniería Ryduan Maximiliano Arévalo Castellano<br>
         <b>Contacto:</b> <a href="mailto:ryduare@gmail.com">ryduare@gmail.com</a>
     </div>
